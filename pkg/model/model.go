384a7bcc5fc46b29d9f97a99933456528707c746
Alter <tuuchan0420@gmail.com>
2024-06-15 10:10:24 +0900

    model.goの実装

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,4 +1,5 @@
 package model
 
-// db.imageを参考にslides structとgenre structを書いて欲しいです(nullとかも反映させてくださると:tasukaru:)
+// image.pngを参考にslides structとgenre structを書いて欲しいです(nullとかも反映させてくださると:tasukaru:)
+
 
