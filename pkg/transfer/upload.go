453106132b3ce083afd3855540c53832a9305de4
Alter <tuuchan0420@gmail.com>
2024-06-15 18:56:38 +0900

    一部コメントアウトの消去

--- pkg/transfer/upload.go
+++ pkg/transfer/upload.go
@@ -8,50 +8,41 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// IUploadImageService .
 type ITransferFileService interface {
 	UploadFile(c echo.Context) error
 }
 
-// uploadImageService .
 type transferFileService struct {
 	uu storage.ITransferFile
 }
 
-// NewUploadImageService はUploadImageServiceインスタンスを生成
 func Service(uu storage.ITransferFile) ITransferFileService {
 	return &transferFileService{uu}
 }
 
-// HandleUploadImage .
 func (h *transferFileService) UploadFile(ctx echo.Context) error {
-	// ファイルをリクエストから取得
-	// このサンプルでは、「image」というパラメータで送られてくることを想定
-	file, err := ctx.FormFile("image")
+
+	file, err := ctx.FormFile("file")
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid file")
 	}
 
-	// ファイルの開閉処理
 	src, err := file.Open()
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "Could not open file")
 	}
 	defer src.Close()
 
-	// データを読み込み
 	fileData, err := io.ReadAll(src)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "Could not read file")
 	}
 
-	// usecaseのロジックを呼び出す
 	fileName := file.Filename
 	url, err := h.uu.UploadFile(ctx.Request().Context(), fileData, fileName)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "Error uploading image: "+err.Error())
 	}
 
-	// リクエスト元が参照できるURLの文字列が欲しいので、Stringで返却する
 	return ctx.String(http.StatusOK, url)
 }
--- storage/upload.go
+++ storage/upload.go
@@ -8,18 +8,15 @@ import (
 	"os"
 )
 
-// ITransferFile
 type ITransferFile interface {
 	UploadFile(ctx context.Context, fileData []byte, fileName string) (string, error)
 	// ここにdownloadも書く
 }
 
 type TransferFileService struct {
-	// storage.goで作った構造体を受け取る
 	storageClient *firebase.FirebaseStorage
 }
 
-// NewUploadFileService は UploadFileService の新しいインスタンスを生成
 func NewTransferFileService(ctx context.Context) (ITransferFile, error) {
 	// firebaseAppの初期化
 	firebaseApp, err := firebase.InitFirebaseApp(ctx)
@@ -38,16 +35,15 @@ func NewTransferFileService(ctx context.Context) (ITransferFile, error) {
 	}, nil
 }
 
-// UploadFile は画像データを受け取り、それを外部サービスへアップロードし、アップロードされた画像のURLを返却する
 func (t *TransferFileService) UploadFile(ctx context.Context, fileData []byte, fileName string) (string, error) {
 	if len(fileData) == 0 {
 		return "", errors.New("file data is empty")
 	}
 
-	path := fmt.Sprintf("files/%s", fileName) // 画像の保存先パスを構築、ディレクトリ中の/images/の中に保存される
+	path := fmt.Sprintf("files/%s", fileName)
 	bucketName := os.Getenv("FIREBASE_STORAGE_BUCKET")
 
-	// sdk/firebase/storage.goの保存処理を呼び出す
+	// firebase/storage.goの保存処理を呼び出す
 	url, err := t.storageClient.Upload(ctx, bucketName, fileData, path)
 	if err != nil {
 		return "", fmt.Errorf("failed to upload image to Firebase Storage: %v", err)
