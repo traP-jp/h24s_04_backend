50b2ced43dc45f75547ca31fa6f7c47ddbf19a2a
Alter <tuuchan0420@gmail.com>
2024-06-15 12:32:08 +0900

    modelの修正

--- pkg/genre/genre.go
+++ pkg/genre/genre.go
@@ -2,7 +2,6 @@ package genre
 
 import (
 	"fmt"
-	"h24s_04/pkg/model"
 	"net/http"
 
 	"github.com/jmoiron/sqlx"
@@ -13,21 +12,26 @@ type GenreService struct {
 	db *sqlx.DB
 }
 
+type Genre struct {
+	id        string `json:"id,omitempty"  db:"id"`
+	genrename string `json:"genrename,omitempty"  db:"genrename"`
+}
+
 func Service(db *sqlx.DB) *GenreService {
 	return &GenreService{db: db}
 
 }
 
-func (s *GenreService) GetGenres(ctx echo.Context) error {
-	// var genres []model.Genre みたいに定義してくれれば
-	// ジャンル一覧を返す jsonに入れればできそう
+// func (s *GenreService) GetGenres(ctx echo.Context) error {
+// 	// var genres []model.Genre みたいに定義してくれれば
+// 	// ジャンル一覧を返す jsonに入れればできそう
 
-}
+// }
 
 func (s *GenreService) PostGenres(ctx echo.Context) error {
 	// var genre *model.Genre みたいに定義してerr:=ctx.Bind(genre)みたいに
 	// 登録したジャンルを返す
-	var genre *model.Genre
+	genre := &Genre{}
 	err := ctx.Bind(genre)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("%+v", err))
@@ -37,14 +41,14 @@ func (s *GenreService) PostGenres(ctx echo.Context) error {
 
 }
 
-func (s *GenreService) GetGenresGenreid(ctx echo.Context) error {
+// func (s *GenreService) GetGenresGenreid(ctx echo.Context) error {
 
-}
+// }
 
-func (s *GenreService) PatchGenresGenreid(ctx echo.Context) error {
+// func (s *GenreService) PatchGenresGenreid(ctx echo.Context) error {
 
-}
+// }
 
-func (s *GenreService) DeleteGenresGenreid(ctx echo.Context) error {
+// func (s *GenreService) DeleteGenresGenreid(ctx echo.Context) error {
 
-}
+// }
