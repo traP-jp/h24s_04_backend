8d92438aafb6fac0beaea568118a484709256d7f
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2024-06-15 09:44:29 +0900

    struct Slide, Genre

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,4 +1,23 @@
 package model
 
+import (
+	"database/sql"
+	"time"
+)
+
 // db.imageを参考にslides structとgenre structを書いて欲しいです(nullとかも反映させてくださると:tasukaru:)
 
+type Slide struct {
+	id          string         `json:"id,omitempty"  db:"id"`
+	dl_url      string         `json:"dl_url,omitempty"  db:"dl_url"`
+	thumb_url   sql.NullString `json:"thumb,omitempty"  db:"thumb"`
+	title       string         `json:"title,omitempty"  db:"title"`
+	genre_id    string         `json:"genre_id,omitempty"  db:"genre_id"`
+	posted_at   time.Time      `json:"posted_at,omitempty"  db:"posted_at"`
+	description sql.NullString `json:"description,omitempty"  db:"description"`
+}
+
+type Genre struct {
+	id        string `json:"id,omitempty"  db:"id"`
+	genrename string `json:"genrename,omitempty"  db:"genrename"`
+}
