d252c87b8c3adbfd1a36e2f862fd2106d6e3e0a6
Alter <tuuchan0420@gmail.com>
2024-06-16 00:03:41 +0900

    godotenvを除いた

--- firebase/firebase.go
+++ firebase/firebase.go
@@ -6,16 +6,10 @@ import (
 	"os"
 
 	firebase "firebase.google.com/go/v4"
-	"github.com/joho/godotenv"
 	"google.golang.org/api/option"
 )
 
 func InitFirebaseApp(ctx context.Context) (*firebase.App, error) {
-	if err := godotenv.Load(); err != nil {
-		fmt.Println("No .env file found")
-
-		return nil, fmt.Errorf("failed to load .env file: %v", err)
-	}
 
 	// 環境変数からアカウント情報を読み込み
 	serviceAccount := os.Getenv("FIREBASE_SERVICE_ACCOUNT")
