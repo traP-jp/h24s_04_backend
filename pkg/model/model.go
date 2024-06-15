0924446cebcade571b67449c794728af46db8445
Alter <tuuchan0420@gmail.com>
2024-06-15 12:32:07 +0900

    modelの修正

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -9,16 +9,16 @@ import (
 // image.pngを参考にslides structとgenre structを書いて欲しいです(nullとかも反映させてくださると:tasukaru:)
 
 type Slide struct {
-	id          string         `json:"id,omitempty"  db:"id"`
-	dl_url      string         `json:"dl_url,omitempty"  db:"dl_url"`
-	thumb_url   sql.NullString `json:"thumb_url,omitempty"  db:"thumb"`
-	title       string         `json:"title,omitempty"  db:"title"`
-	genre_id    string         `json:"genre_id,omitempty"  db:"genre_id"`
-	posted_at   time.Time      `json:"posted_at,omitempty"  db:"posted_at"`
-	description sql.NullString `json:"description,omitempty"  db:"description"`
+	Id          string         `json:"id,omitempty"  db:"id"`
+	DL_url      string         `json:"dl_url,omitempty"  db:"dl_url"`
+	Thumb_url   sql.NullString `json:"thumb_url,omitempty"  db:"thumb"`
+	Title       string         `json:"title,omitempty"  db:"title"`
+	Genre_id    string         `json:"genre_id,omitempty"  db:"genre_id"`
+	Posted_at   time.Time      `json:"posted_at,omitempty"  db:"posted_at"`
+	Description sql.NullString `json:"description,omitempty"  db:"description"`
 }
 
 type Genre struct {
-	id        string `json:"id,omitempty"  db:"id"`
-	genrename string `json:"genrename,omitempty"  db:"genrename"`
+	Id        string `json:"id,omitempty"  db:"id"`
+	Genrename string `json:"genrename,omitempty"  db:"genrename"`
 }
