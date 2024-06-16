936dfb93d4675ce7e41c9051b4bc4365cd2f773e
Alter <tuuchan0420@gmail.com>
2024-06-16 12:18:46 +0900

    model.goのsql.NULLstringをstringに

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"database/sql"
 	"time"
 
 	"github.com/gofrs/uuid"
@@ -10,15 +9,15 @@ import (
 // image.pngを参考にslides structとgenre structを書いて欲しいです(nullとかも反映させてくださると:tasukaru:)
 
 type Slide struct {
-	Id             uuid.UUID      `json:"id,omitempty"  db:"id"`
-	DL_url         string         `json:"dl_url,omitempty"  db:"dl_url"`
-	Thumb_url      sql.NullString `json:"thumb_url,omitempty"  db:"thumb_url"`
-	Title          string         `json:"title,omitempty"  db:"title"`
-	Genre_id       uuid.UUID      `json:"genre_id,omitempty"  db:"genre_id"`
-	Posted_at      time.Time      `json:"posted_at,omitempty"  db:"posted_at"`
-	Description    sql.NullString `json:"description,omitempty"  db:"description"`
-	URL_updated_at time.Time      `json:"url_updated_at,omitempty"  db:"url_updated_at"`
-	Filepath       string         `json:"filepath,omitempty"  db:"filepath"`
+	Id             uuid.UUID `json:"id,omitempty"  db:"id"`
+	DL_url         string    `json:"dl_url,omitempty"  db:"dl_url"`
+	Thumb_url      string    `json:"thumb_url,omitempty"  db:"thumb_url"`
+	Title          string    `json:"title,omitempty"  db:"title"`
+	Genre_id       uuid.UUID `json:"genre_id,omitempty"  db:"genre_id"`
+	Posted_at      time.Time `json:"posted_at,omitempty"  db:"posted_at"`
+	Description    string    `json:"description,omitempty"  db:"description"`
+	URL_updated_at time.Time `json:"url_updated_at,omitempty"  db:"url_updated_at"`
+	Filepath       string    `json:"filepath,omitempty"  db:"filepath"`
 }
 
 type Genre struct {
